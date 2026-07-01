15067c17dc577a20deced09a0943151eec957b56
Sangeeth Amirthanathan <zansangeeth17@gmail.com>
2026-07-01 13:23:42 +0100

    changed text style for create quotation

--- app/src/main/java/com/sangeeth/evesgifts/ui/home/HomeScreen.kt
+++ app/src/main/java/com/sangeeth/evesgifts/ui/home/HomeScreen.kt
@@ -10,18 +10,23 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+
+@Preview
 @Composable
 fun HomeScreen(){
     Column(modifier = Modifier.padding(20.dp)) {
         Text(
             text = "Create Quotation",
             modifier = Modifier.fillMaxWidth(),
-            textAlign = TextAlign.Center,
-            fontSize = 40.sp
+            textAlign = TextAlign.Left,
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold
         )
     }
 }
